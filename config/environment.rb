6e573613001f4b959545a5d1ddd7237d2544daa9
github username <github email address>
2017-11-17 09:58:49 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ Bundler.require
 require 'active_record'
 require 'rake'
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+require_all 'app'
+require_all 'lib'
 
 DBRegistry[ENV["PLAYLISTER_ENV"]].connect!
 DB = ActiveRecord::Base.connection
@@ -19,7 +19,7 @@ def migrate_db
     DB.execute("DROP TABLE #{table}")
   end
 
-  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].each do |f| 
+  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].each do |f|
     require f
     migration = Kernel.const_get(f.split("/").last.split(".rb").first.gsub(/\d+/, "").split("_").collect{|w| w.strip.capitalize}.join())
     migration.migrate(:up)
